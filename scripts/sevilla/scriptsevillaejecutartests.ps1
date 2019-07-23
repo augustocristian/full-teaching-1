84d3ef301e948ef6fb4c4e672396d5d3b702c88d
Augusto <augustocristian@uniovi.es>
2019-07-24 00:19:06 +0200

    Adding some scripts

---
+++ scripts/sevilla/scriptsevillaejecutartests.ps1
@@ -0,0 +1,6 @@
+
+cd C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest
+mvn install -DskipTests
+java -jar C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target
+
+
