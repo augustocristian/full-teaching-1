2a2eaf151edbfdbee26245da9a04ced70971bcc5
Augusto <augustocristian@uniovi.es>
2019-07-23 23:43:41 +0200

    Adding sevilla scripts

---
+++ scripts/sevilla/scriptsevilla.ps1
@@ -0,0 +1,7 @@
+mkdir C:/sevillarepos
+cd C:/sevillarepos
+git clone https://github.com/augustocristian/full-teaching-tunon-tests.git
+git clone https://github.com/OpenVidu/full-teaching.git
+cd full-teaching
+docker-compose up
+cd ..
