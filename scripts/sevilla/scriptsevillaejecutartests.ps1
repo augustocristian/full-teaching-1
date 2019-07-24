9b7f430ae19a81131d6fd6c642e43ed3cd4a7b08
Augusto <augustocristian@uniovi.es>
2019-07-24 12:27:27 +0200

    Adding the new port and tests

--- scripts/sevilla/scriptsevillaejecutartests.ps1
+++ scripts/sevilla/scriptsevillaejecutartests.ps1
@@ -1,9 +1,7 @@
 
 cd C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest
 mvn install -DskipTests
-java -cp e2e.no-elastest-0.0.2-SNAPSHOT-jar-with-dependencies.jar -Xms256m -Xmx512m -jar e2e.no-elastest-0.0.2-SNAPSHOT-tests.jar
-
-java -Xms256m -Xmx512m -jar  C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT.jar "1GBRAM"
+java -cp C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-jar-with-dependencies.jar -Xms256m -Xmx512m -jar C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-tests.jar
 
 
 
