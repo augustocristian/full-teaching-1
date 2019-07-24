79c81c99d77ea47b3df6c2b7727d2b6cbf166439
Augusto <augustocristian@uniovi.es>
2019-07-24 15:04:21 +0200

    Adding some things to the scripts

--- scripts/sevilla/scriptsevillaejecutartests.ps1
+++ scripts/sevilla/scriptsevillaejecutartests.ps1
@@ -7,13 +7,23 @@ $x = @("800","1920")
 $y = @("600","1080")
 $menmax = @("-Xms256m","-Xms512m")
 $memin = @("-Xms256m","-Xmx512m")
+$processafinity = @('0001','0111')
+For($p=0; $p -le 1; $p++){
 For($i=0; $i -le 1; $i++){
 For ($j=0; $j -le 1; $j++){
     C:\Users\crist\Escritorio\full-teaching-tunon-tests\scripts\sevilla\QRes.exe /x:$($x[$i]) /y:$($y[$i])
 
 Start-Sleep 5
 
-java -cp C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-jar-with-dependencies.jar $($memin[$j]) $($menmax[$j]) -jar C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-tests.jar
+$app_name = "java"
+$app_arguments = "-cp C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-jar-with-dependencies.jar $($memin[$j]) $($menmax[$j]) -jar C:/Users/crist/Escritorio/full-teaching-tunon-tests/e2e-test/no-Elastest/target/e2e.no-elastest-0.0.2-SNAPSHOT-tests.jar"
 
+$app = Start-Process $app_name $app_arguments -PassThru 
+echo "sin afinidad"
+$app.ProcessorAffinity = [System.IntPtr][Int]$processafinity[$p]
+echo "metido afinidad "
+Wait-Process -Id $app.Id
+
+}
 }
 }
