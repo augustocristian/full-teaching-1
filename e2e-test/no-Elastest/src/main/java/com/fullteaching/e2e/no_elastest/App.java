cafacc0a3f9f4c34075e08046928ca0f387b7fa6
Augusto <augustocristian@uniovi.es>
2020-06-07 21:16:46 +0200

    Adding some extra files

--- e2e-test/no-Elastest/src/main/java/com/fullteaching/e2e/no_elastest/App.java
+++ e2e-test/no-Elastest/src/main/java/com/fullteaching/e2e/no_elastest/App.java
@@ -1,10 +1,20 @@
 package com.fullteaching.e2e.no_elastest;
+import retorch.exceptions.NoFinalActivitiesException;
+import retorch.exceptions.NoTGroupsInTheSchedulerException;
+import retorch.exceptions.NotValidSystemException;
+import retorch.exceptions.classifier.EmptyInputException;
+import retorch.main.OrchestrationGenericToolBox;
+import retorch.resourceidentification.RetorchSerializer;
+
+import java.io.IOException;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+
+
 
     }
 }
